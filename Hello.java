8f3adb1e3a5982caa473affca36e4bc3172f63dc
chen <chen-jh@outlook.com>
2018-10-11 18:09:13 +0800

    修改编码为utf-8

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("hello github");
-    System.out.println("ÊÕµ½ÁË");
+    System.out.println("æ”¶åˆ°äº†");
   }
 }
